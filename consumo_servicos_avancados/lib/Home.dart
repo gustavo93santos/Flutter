f464a0f526d4fe6fba9444ea7816f82e5c8ad60b
Gustavo Santos <gustavo93santos@gmail.com>
2020-02-20 00:27:53 -0300

    Post

--- consumo_servicos_avancados/lib/Home.dart
+++ consumo_servicos_avancados/lib/Home.dart
@@ -5,72 +5,102 @@ import 'dart:convert';
 import 'dart:async';
 
 class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
+	@override
+	_HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
 
-  String urlBase = "https://jsonplaceholder.typicode.com";
+	String urlBase = "https://jsonplaceholder.typicode.com";
 
-  Future <List<Post>>_recuperarPostagens() async{
+	Future <List<Post>>_recuperarPostagens() async{
 
-    http.Response response = await http.get(urlBase+ "/posts");
-    var dadosJson = json.decode(response.body);
+		http.Response response = await http.get(urlBase+ "/posts");
+		var dadosJson = json.decode(response.body);
 
-    List<Post> postagens = List();
+		List<Post> postagens = List();
 
-    for(var post in dadosJson){
-      Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
-      postagens.add(p);
-    }
-    return postagens;
-  }
+		for(var post in dadosJson){
+			Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
+			postagens.add(p);
+		}
+		return postagens;
+	}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Consumo de serviços avançados"),
-      ),
-      body:
-      FutureBuilder<List<Post>>(
-        future: _recuperarPostagens(),
-        builder: (context, snapshot){
+	_post() {
 
-          switch( snapshot.connectionState ){
-            case ConnectionState.none :
-            case ConnectionState.waiting :
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-              break;
-            case ConnectionState.active :
-            case ConnectionState.done :
-              print("conexao done");
-              if( snapshot.hasError ){
-                print("Erro ao carregar os dados.");
-              }else {
-                print("Lista Carregou!!");
-                return 
-                ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index){
+	}
+	
+	_put() {
 
-                    List<Post> lista = snapshot.data;
-                    Post post = lista[index];
+	}
+	
+	_patch() {
 
-                    return ListTile(
-                      title: Text(post.title),
-                      subtitle: Text(post.body),
-                    );
-                  }
-                );
-              }
-              break;
-          }
-        },
-      ),
-    );
-  }
+	}
+
+	_delete() {
+
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text("Consumo de serviços avançados"),
+			),
+			body: Container(
+				padding: EdgeInsets.all(16),
+				child: Column(
+					children: <Widget>[
+						Row(
+							children: <Widget>[
+								RaisedButton(
+									child: Text("Salvar"),
+									onPressed: _post)
+							],
+						),
+						Expanded(
+							child: FutureBuilder<List<Post>>(
+								future: _recuperarPostagens(),
+								builder: (context, snapshot){
+									switch( snapshot.connectionState ){
+										case ConnectionState.none :
+										case ConnectionState.waiting :
+											return Center(
+												child: CircularProgressIndicator(),
+											);
+											break;
+										case ConnectionState.active :
+										case ConnectionState.done :
+											if( snapshot.hasError ){
+												print("lista: Erro ao carregar ");
+											}else {
+												return ListView.builder(
+														itemCount: snapshot.data.length,
+														itemBuilder: (context, index){
+
+															List<Post> lista = snapshot.data;
+															Post post = lista[index];
+
+															return ListTile(
+																title: Text( post.title ),
+																subtitle: Text( post.id.toString() ),
+															);
+
+														}
+												);
+
+											}
+											break;
+									}
+								},
+							),
+						)
+					],
+				),
+			)
+			
+		);
+	}
 }
